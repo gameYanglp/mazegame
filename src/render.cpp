286f55d7f2067499a28f606b719428f6dd38c712
yanglp2005 <yanglp2005@gmail.com>
2015-05-23 18:21:50 +0800

    modify the rendering perspective

--- src/render.cpp
+++ src/render.cpp
@@ -8,14 +8,20 @@ void render::run(float3** colors, int n)
 	glClearColor( 0.0f, 0.0f, 0.0f, 1.0f );
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+	glPushMatrix();
+
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective( 45, 1, 0.1, 10 );
+	//gluPerspective( 90, 1, 0.1, 10 );
+	glOrtho(-0.5f, 0.5f, -0.5f, 0.5f, 0, 12.0f );
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	gluLookAt( 0.5, -0.5, 1, 0.5, -0.5, 0, 0, 1, 0 );
+	gluLookAt( 0.5, -0.5, 1.5, 0.5, -0.5, 0, 0, 1, 0 );
 
+	glViewport(0,0,600,600);
+
+	//n=1;
 	float cell = 1.0f/n;
 	//render the rectangles here.
 	for( int i=0; i<n; i++ ) for( int j=0; j<n; j++ )
@@ -29,6 +35,7 @@ void render::run(float3** colors, int n)
 		glVertex3f(j*cell, -i*cell-cell, 0.0);
 		glEnd();
 	}
+	glPopMatrix();
 
 	glutPostRedisplay();
 	glutSwapBuffers();
